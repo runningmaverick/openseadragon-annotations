2135fd007b55965f38f3b0160b1db9249610c842
j-hudecek <j.hudecek@nki.nl>
2016-06-15 12:42:55 +0200

    Update stroke-width based on zoom level
    #6

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -10,6 +10,12 @@ export default class Controls extends OpenSeadragon.EventSource {
   initialize(viewer, options) {
     var options = options || {};
     this.viewer = viewer;
+    viewer.addHandler('zoom', function(e) { 
+			var width = 0.5;
+			var paths = document.getElementsByTagName('path');
+			if (paths.length > 0)
+				paths[0].setAttribute("stroke-width", width/e.zoom)
+		})
     if (options.controls) { options.controls.forEach(this.add.bind(this)); }
     return this;
   }
